9e53f94e6ceacca133e25555d1bcdb44fcc1da58
Raz Rotenberg <raz.rotenberg@gmail.com>
2022-09-10 01:43:58 +0300

    Auto-refreshing envs and devices views every 30 seconds
    * Currently this is hard-coded. To be configurable in the future.

--- src/extension.ts
+++ src/extension.ts
@@ -9,4 +9,17 @@ export async function activate(context: vscode.ExtensionContext) {
   env.init(context);
   envs.init(context);
   devices.init(context);
+
+  // TODO(raz): make auto refresh configurable
+  autoRefresh(context);
+}
+
+function autoRefresh(context: vscode.ExtensionContext) {
+  let interval = setInterval(() => {
+    vscode.commands.executeCommand('genv.envs.refresh');
+    vscode.commands.executeCommand('genv.devices.refresh');
+  }, 30000); // TODO(raz): make the refresh interval configurable
+
+  // TODO(raz): is this correct? should we use 'dispose()' for this?
+  context.subscriptions.push({ dispose() { clearInterval(interval); }});
 }
